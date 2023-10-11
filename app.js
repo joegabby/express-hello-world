ea45352920b26d5492f3d41a7beb2297b6d35ea2
joegabby <gabriel.joe56@gmail.com>
2023-10-11 16:49:26 +0100

    first

--- app.js
+++ app.js
@@ -9,6 +9,48 @@ const server = app.listen(port, () => console.log(`Example app listening on port
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
+const container = document.querySelector(".container")
+const coffees = [
+  { name: "Perspiciatis", image: "Capturem.jpg" },
+  { name: "Voluptatem", image: "Capturem.jpg" },
+  { name: "Explicabo", image: "Capturem.jpg" },
+  { name: "Rchitecto", image: "Capturem.jpg" },
+  { name: " Beatae", image: "Capturem.jpg" },
+  { name: " Vitae", image: "Capturem.jpg" },
+  { name: "Inventore", image: "Capturem.jpg" },
+  { name: "Veritatis", image: "Capturem.jpg" },
+  { name: "Accusantium", image: "Capturem.jpg" },
+]
+const showCoffees = () => {
+    let output = ""
+    coffees.forEach(
+      ({ name, image }) =>
+        (output += `
+                <div class="card">
+                  <img class="card--avatar" src=${image} />
+                  <h1 class="card--title">${name}</h1>
+                  <a class="card--link" href="#">Taste</a>
+                </div>
+                `)
+    )
+    container.innerHTML = output
+  }
+  
+  document.addEventListener("DOMContentLoaded", showCoffees)
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
+
+
+
+
+
 const html = `
 <!DOCTYPE html>
 <html>
